dc509c0b155114c08bba5fa2d6d11a82a9498f11
dttinh18it3 <dttinh.18it3@sict.udn.vn>
2021-04-18 21:54:09 +0700

    main.py : format code

--- TAV/main.py
+++ TAV/main.py
@@ -1,11 +1,12 @@
-
 from PyQt5 import QtWidgets, uic
 import sys
+
 
 class Dashboard(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi("views/dashboard.ui", self)  # Press Ctrl+F8 to toggle the breakpoint.
+
 
 app = QtWidgets.QApplication([])
 win = Dashboard()
