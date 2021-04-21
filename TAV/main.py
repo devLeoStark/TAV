72a5a7af8b3963a5e379f0a2357c26ba3d0200f2
dttinh18it3 <dttinh.18it3@sict.udn.vn>
2021-04-22 00:16:48 +0700

    Quick Scan Processing

--- TAV/main.py
+++ TAV/main.py
@@ -1,9 +1,18 @@
+import hashlib
+import json
 import os
+import time
 
 from PyQt5 import QtWidgets, uic
+from PyQt5.QtGui import QColor
+from PyQt5.QtWidgets import QMessageBox
 import sys
 
 from PyQt5.QtWidgets import QFileDialog
+
+COLOR_BLACK = QColor(0, 0, 0)
+COLOR_RED = QColor(255, 0, 0)
+COLOR_GREEN = QColor(0, 120, 0)
 
 
 class Dashboard(QtWidgets.QMainWindow):
@@ -18,12 +27,95 @@ class Dashboard(QtWidgets.QMainWindow):
         self.disableSpecificScan()
         self.optionSpecificScan.clicked.connect(self.enableSpecificScan)
         self.optionFullScan.clicked.connect(self.disableSpecificScan)
+        self.isScanning(False)
+        self.btnScan.clicked.connect(self.QuickScan)
+
+    def QuickScan(self):
+        self.ClearLog()
+        if self.optionFullScan.isChecked():
+            path = "C:/"
+            self.FullScan(path)
+        else:
+            if self.folderPath.text() == "":
+                QMessageBox.warning(self, 'Alert', "Please choose your folder", QMessageBox.Yes, QMessageBox.Yes)
+            elif not self.checkPath(self.folderPath.text()):
+                QMessageBox.warning(self, 'Alert', "Folder is invalid", QMessageBox.Yes, QMessageBox.Yes)
+            else:
+                path = self.folderPath.text()
+                self.SpecificScan(path)
+
+    def SpecificScan(self, path):
+        count = 0
+        isVirus = False
+        scanned_size = 0
+        total_size = self.TotalSizeDirectory(path)
+        database = self.ReadJsonData("database/data.json")
+
+        self.isScanning(True)
+        self.logBrowser.append("Scanning: " + path)
+        self.logBrowser.append(
+            ".......................................................................................")
+        scanPath = path.replace('/', '\\')
+        for path, directories, files in os.walk(path):
+            for file in files:
+                if file.endswith("exe"):
+                    filePath = os.path.join(path, file)
+                    fileToMD5 = self.HashFileMD5(filePath)
+                    scanned_size += os.path.getsize(filePath)
+                    for data in database:
+                        if fileToMD5 == data["md5"]:
+                            isVirus = True
+                    self.LoggingProcess(filePath, isVirus)
+                    self.progressBarScan.setValue((int(scanned_size / total_size)) * 100)
+        self.Finish()
+
+    def Finish(self):
+        self.logBrowser.setTextColor(COLOR_BLACK)
+        self.logBrowser.append("FINISH")
+
+    def TotalSizeDirectory(self, path):
+        total_size = 0
+        for path, directories, files in os.walk(path):
+            filePath = ""
+            for file in files:
+                if file.endswith("exe"):
+                    total_size += os.path.getsize(os.path.join(path, file))
+        return total_size
+
+    def LoggingProcess(self, path, isVirus):
+        logLine = time.strftime("%Y/%m/%d - %H:%M:%S", time.localtime()) + ": " + path
+        if not isVirus:
+            self.logBrowser.setTextColor(COLOR_GREEN)
+            self.logBrowser.append(logLine)
+        else:
+            self.logBrowser.setTextColor(COLOR_RED)
+            self.logBrowser.append(logLine)
+
+    def ReadJsonData(self, dataPath):
+        with open(dataPath) as json_file:
+            jsonData = json.load(json_file)
+            return jsonData
+
+    def HashFileMD5(self, file):
+        md5 = hashlib.md5()
+        with open(file, 'rb') as file:
+            buffer = file.read()
+            md5.update(buffer)
+        return md5.hexdigest()
+
+    # def Progressing(self):
+
+    def FullScan(self, path):
+        self.isScanning(True)
+        self.logBrowser.append("Full Scanning: " + path)
 
     def enableSpecificScan(self):
+        self.isScanning(False)
         self.folderPath.setVisible(True)
         self.btnChooseFolder.setVisible(True)
 
     def disableSpecificScan(self):
+        self.isScanning(False)
         self.folderPath.setVisible(False)
         self.btnChooseFolder.setVisible(False)
 
@@ -39,6 +131,35 @@ class Dashboard(QtWidgets.QMainWindow):
     def chooseFolder(self):
         folderName = QFileDialog.getExistingDirectory(self, "Choose Folder")
         self.folderPath.setText(folderName)
+
+    def checkPath(self, path):
+        return os.path.exists(path)
+
+    def EnableLogToolBar(self, boolean):
+        if boolean:
+            self.btnCancel.setVisible(True)
+            self.btnClearLog.setVisible(True)
+            self.progressBarScan.setVisible(True)
+            self.btnCancel.clicked.connect(self.CancelScan)
+            self.btnClearLog.clicked.connect(self.ClearLog)
+        else:
+            self.btnCancel.setVisible(False)
+            self.btnClearLog.setVisible(False)
+            self.progressBarScan.setVisible(False)
+
+    def isScanning(self, boolean):
+        if boolean:
+            self.EnableLogToolBar(True)
+        else:
+            self.EnableLogToolBar(False)
+
+    def CancelScan(self):
+        self.isScanning(False)
+        self.logBrowser.setTextColor(COLOR_BLACK)
+        self.logBrowser.append("CANCELED SCAN\n")
+
+    def ClearLog(self):
+        self.logBrowser.clear()
 
 
 app = QtWidgets.QApplication(sys.argv)
